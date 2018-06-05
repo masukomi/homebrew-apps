436a6f98a57d946a010d7b67d42c31700e1b7a58
masukomi <masukomi@masukomi.org>
2018-06-05 13:42:01 -0400

    [oho] updated to v1.1.1

--- Formula/oho.rb
+++ Formula/oho.rb
@@ -1,8 +1,8 @@
 class Oho < Formula
   desc "Takes your colorful terminal output and converts it to HTML for sharing"
   homepage "https://github.com/masukomi/oho"
-  url "https://github.com/masukomi/oho/releases/download/v1.1.0/oho_v1.1.0.tgz"
-  sha256 "ccad6901ba81e19c2e46125826b06281fbe4b01f2f1518d5dcac98fc4da528b7"
+  url "https://github.com/masukomi/oho/releases/download/v1.1.1/oho_v1.1.1.tgz"
+  sha256 "c1cd3547e482711566e681a7bc01e518b7d8209d31105d8772c98a025f0f5b89"
 
   depends_on "bdw-gc"
   depends_on "libevent"
