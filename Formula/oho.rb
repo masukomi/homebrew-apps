6e0e38961d76cdfe6197365cb6ab67a480236209
masukomi <masukomi@masukomi.org>
2018-06-16 15:53:59 -0400

    updated oho to v1.3.0

--- Formula/oho.rb
+++ Formula/oho.rb
@@ -1,8 +1,8 @@
 class Oho < Formula
   desc "Takes your colorful terminal output and converts it to HTML for sharing"
   homepage "https://github.com/masukomi/oho"
-  url "https://github.com/masukomi/oho/releases/download/v1.2.0/oho_v1.2.0.tgz"
-  sha256 "337273a9e48b075b8e7963c8a601268c023b7f4fc5f30a185a406beee256a98f"
+  url "https://github.com/masukomi/oho/releases/download/v1.3.0/oho_v1.3.0.tgz"
+  sha256 "8bc0942045f9c657189e8ed6e2973021b4b311a08a9f900e9d1d03295bb39485"
 
   depends_on "bdw-gc"
   depends_on "libevent"
@@ -12,8 +12,8 @@ class Oho < Formula
   end
 
   test do
-    out = pipe_output(bin/"oho", "[35mrain[34mpill[00m")
-    style_regexp = %r{<span style="color: fuchsia; ">rain<\/span><span style="color: #3333FF; ">pill<\/span>}
+    out = pipe_output(bin/"oho", "[35mno[34mfascism[00m")
+    style_regexp = %r{<span style="color: fuchsia; ">no<\/span><span style="color: #3333FF; ">fascism<\/span>}
     assert_match(style_regexp, out)
   end
 end
