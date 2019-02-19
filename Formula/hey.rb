0572b34004a57e8c27126644d16cd33347e6a6a8
masukomi <masukomi@masukomi.org>
2019-02-19 17:01:16 -0500

    updated hey to v2.0.0-a2

--- Formula/hey.rb
+++ Formula/hey.rb
@@ -1,12 +1,12 @@
 class Hey < Formula
   desc "Hey allows you to track your interruptions as they occur."
   homepage "https://interrupttracker.com"
-  current_version="2.0.0-a1"
+  current_version="2.0.0-a2"
   # url
   # "https://github.com/masukomi/hey/releases/download/v#{current_version}/hey_v#{current_version}.tgz"
   url "https://github.com/masukomi/hey_2/releases/download/v#{current_version}/hey_v#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/hey_2/hey_v#{current_version}.tgz"
-  sha256 "42fbfd26a60a5c7c90701dde2f72f3c70463c26148b474d4d92aae03d428d97f"
+  sha256 "ac761861290efd4c9a848b06746662f551b7717dc1501c08fe54f3469c038222"
 
   depends_on "bdw-gc"
   depends_on "libevent"
