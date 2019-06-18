10f3264ca535b122fb084472bcb4f1240a4a8002
masukomi <masukomi@masukomi.org>
2019-06-18 18:59:22 -0400

    new version of days_progress

--- Formula/days_progress.rb
+++ Formula/days_progress.rb
@@ -1,21 +1,14 @@
 class DaysProgress < Formula
   desc "A simple command line chart of your progress through the day"
   homepage "https://github.com/masukomi/days_progress"
-  current_version="v1.0.0"
+  current_version="v1.0.1"
   url "https://github.com/masukomi/days_progress/releases/download/#{current_version}/days_progress_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/days_progress/days_progress_#{current_version}.tgz"
-  sha256 "020a612472b76b0e2896ecf3bf61bd04c3d451937b8542c7fa67715df6394db8"
+  sha256 "f96046a80db7d000c5a2e5fa293694bb5afcc011f76d81700db2d686e6d6c350"
 
   depends_on "chicken"
 
   def install
-# commented out because Homebrew says
-# install: /usr/local/Cellar/chicken/5.0.0/lib/chicken/9/filepath.o: Operation not permitted
-# which may be macOS and may be homebrew. Not sure... 
-# going to instruct people to run this manually in the README
-#    system "chicken-install", "filepath"
-#    system "chicken-install", "simple-loops"
-#    system "chicken-install", "ansi-escape-sequences"
     bin.install "days_progress"
   end
 
