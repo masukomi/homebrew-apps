15a74c685acaca2bbd3f2a2dec693563aa05f299
masukomi <masukomi@masukomi.org>
2019-07-12 23:01:36 -0400

    updated days_progress to v1.2.0

--- Formula/days_progress.rb
+++ Formula/days_progress.rb
@@ -1,12 +1,10 @@
 class DaysProgress < Formula
   desc "A simple command line chart of your progress through the day"
   homepage "https://github.com/masukomi/days_progress"
-  current_version="v1.1.1"
+  current_version="v1.2.0"
   url "https://github.com/masukomi/days_progress/releases/download/#{current_version}/days_progress_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/days_progress/days_progress_#{current_version}.tgz"
-  sha256 "f8d3ca6e54ff1cc3f1650b85bdfe871c2afa2d56d8222d01db3e5a255c2c0c41"
-
-  #depends_on "chicken"
+  sha256 "742d2c37c74cbd6cb176a73448f956ec93ddf1cef942939484998bd3b8f5839b"
 
   def install
     bin.install "days_progress"
