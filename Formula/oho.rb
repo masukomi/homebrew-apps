cd892713b4f072642006a4e91d101bd2a9f7e0b6
masukomi <masukomi@masukomi.org>
2019-10-15 10:13:07 -0400

    Reverted v1.3.3 to binary distro

--- Formula/oho.rb
+++ Formula/oho.rb
@@ -2,16 +2,15 @@ class Oho < Formula
   desc "Takes your colorful terminal output and converts it to HTML for sharing"
   homepage "https://github.com/masukomi/oho"
   current_version="v1.3.3"
-  url "https://github.com/masukomi/oho/releases/download/#{current_version}/oho_#{current_version}-source.tgz"
+  url "https://github.com/masukomi/oho/releases/download/#{current_version}/oho_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/oho/oho_#{current_version}.tgz"
   sha256 "a3fe9159722d6ea4dd18f57581ffb238f3c57271f1149e83bf4652059f048295"
 
+
   depends_on "bdw-gc"
   depends_on "libevent"
-  depends_on "crystal"
 
   def install
-    system "crystal", "build", "--release", "src/oho.cr"
     bin.install "oho"
   end
 
