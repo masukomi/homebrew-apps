28a34a3081322846aee36cfcf1b88d8fd8a60a44
masukomi <masukomi@masukomi.org>
2020-02-07 17:47:43 -0500

    private comments v1.2.2 release

--- Formula/private_comments.rb
+++ Formula/private_comments.rb
@@ -1,10 +1,10 @@
 class PrivateComments < Formula
-  current_version="v1.2.1"
+  current_version="v1.2.2"
   desc "A REST server to manage private comments on your code."
   homepage "https://github.com/masukomi/private_comments"
   url "https://github.com/masukomi/private_comments/releases/download/#{current_version}/private_comments_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/private_comments/private_comments_#{current_version}.tgz"
-  sha256 "a4593bc96893a85010bcec1b1cebb1e919b716193c64faebf6f22b4d6a7ed3ef"
+  sha256 "98e1132c91728551c2600440f180160796d9d209241f4c91fd7d648344222c9a"
 
   def install
     bin.install "private_comments"
