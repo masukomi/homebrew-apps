fe0f132bad640925e932f91095169bb799893676
masukomi <masukomi@masukomi.org>
2022-03-16 23:17:57 -0400

    updated private_comments & added private_comments_arm v1.2.4

--- Formula/private_comments_arm.rb
+++ Formula/private_comments_arm.rb
@@ -1,10 +1,10 @@
-class PrivateComments < Formula
+class PrivateCommentsArm < Formula
   current_version="v1.2.4"
   desc "A REST server to manage private comments on your code."
   homepage "https://github.com/masukomi/private_comments"
-  url "https://github.com/masukomi/private_comments/releases/download/#{current_version}/private_comments_i386_#{current_version}.tgz"
+  url "https://github.com/masukomi/private_comments/releases/download/#{current_version}/private_comments_arm64_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/private_comments/private_comments_#{current_version}.tgz"
-  sha256 "5829c247af5af660dbe944cbd0061b8bd099c8db63afa7cef44b99f4eddcf1d4"
+  sha256 "ba81a17b20573e1bced8e035e3a0e6577b792d283139b3fcf191f0ddd9de0380"
 
   def install
     bin.install "private_comments"
