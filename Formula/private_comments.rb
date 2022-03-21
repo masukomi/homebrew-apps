5bb27145387cefa6f49e4b863aea4b2f0d498b39
masukomi <masukomi@masukomi.org>
2022-03-21 10:06:47 -0400

    updated private_comments to v1.3.0

--- Formula/private_comments.rb
+++ Formula/private_comments.rb
@@ -1,9 +1,9 @@
 class PrivateComments < Formula
-  current_version="v1.2.4"
+  current_version="v1.3.0"
   architecture=`arch`.strip
   puts "installing for #{architecture} architecture"
-  arm_hash="ba81a17b20573e1bced8e035e3a0e6577b792d283139b3fcf191f0ddd9de0380"
-  intel_hash="5829c247af5af660dbe944cbd0061b8bd099c8db63afa7cef44b99f4eddcf1d4"
+  arm_hash="2b136d21195ff6f4be808e31873352263fe183a47093b4292e22ff8553672e0f"
+  intel_hash="1bc4e5d7fa05811b54a71e085163e079ff3054a83ef6d7031f0b3ef63f3b47d1"
   desc "A REST server to manage private comments on your code."
   homepage "https://github.com/masukomi/private_comments"
   url "https://github.com/masukomi/private_comments/releases/download/#{current_version}/private_comments_#{architecture}_#{current_version}.tgz"
