f8505a050eaf61da900f34b2e0923b7f245266a7
masukomi <masukomi@masukomi.org>
2022-06-03 12:22:15 -0400

    bumped rtest to v1.3.0

--- Formula/rtest.rb
+++ Formula/rtest.rb
@@ -3,10 +3,10 @@
 class Rtest < Formula
   desc 'An RSpec wrapper that reduces noise eases rerunning tests'
   homepage 'https://github.com/masukomi/rtest'
-  current_version = 'v1.2.0'
+  current_version = 'v1.3.0'
   url "https://github.com/masukomi/rtest/releases/download/#{current_version}/rtest_#{current_version}.tgz"
   # url "file:///Users/masukomi/workspace/rtest/rtest_#{current_version}.tgz"
-  sha256 '67047f3034e6cf9cc0db540343f684e7e0b04d42a4dad13f6941117c248b69a4'
+  sha256 '11cd0f867da69ba77c258dff4aa23609928b0d6d43282a1eec7c11761403e335'
 
   def install
     bin.install 'rtest'
