3a29c72aacf239499f138d27d4e676c7a66990bf
masukomi <masukomi@masukomi.org>
2022-10-08 14:48:11 -0400

    [oho] update: new version + builds from source now
    getting errors from homebrew about
    Cask 'oho' is unreadable: wrong constant name #<Class:0x0000000131140af0>
    
    seems to be somehow related to m1 macs.
    VERY unclear what the solution is but it _seem_
    as if
    this works regardless.

--- Formula/oho.rb
+++ Formula/oho.rb
@@ -1,22 +1,26 @@
 class Oho < Formula
   desc "Takes your colorful terminal output and converts it to HTML for sharing"
   homepage "https://github.com/masukomi/oho"
-  current_version="v1.3.5"
-  url "https://github.com/masukomi/oho/releases/download/#{current_version}/oho_#{current_version}.tgz"
-  # url "file:///Users/masukomi/workspace/oho/oho_#{current_version}.tgz"
-  sha256 "f4278211a18c5b14d9099f53457b76da89c36c908d2e89b50ad2c8acd035485e"
-
+  current_version="v1.3.6"
+  url "https://github.com/masukomi/oho/releases/download/#{current_version}/oho_#{current_version}-source.tgz"
+  # url "file:///Users/masukomi/workspace/oho/oho_#{current_version}-src.tgz"
+  sha256 "3a0e0931057ec1b49ef342ad4eda62f268650c5584da521ae144ab6a037ebf95"
 
   depends_on "bdw-gc"
+  depends_on "crystal"
+  depends_on "libev"
   depends_on "libevent"
+  depends_on "llvm"
+  depends_on "pcre"
 
   def install
+    system "crystal", "build", "--release", "src/oho.cr"
     bin.install "oho"
   end
 
   test do
     out = pipe_output(bin/"oho", "[35mno[34mfascism[00m")
-    style_regexp = %r{<span style="color: fuchsia; ">no<\/span><span style="color: #3333FF; ">fascism<\/span>}
+    style_regexp = %r{<span style="color: fuchsia; ">no</span><span style="color: #3333FF; ">fascism</span>}
     assert_match(style_regexp, out)
   end
 end
